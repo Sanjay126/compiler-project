6c69d34a8d3b343caeb36ef3d8a952ab6a7f3322
utshargoel1 <f2016023@Pilani.bits-pilani.ac.in>
2019-03-03 20:03:51 +0530

    hashing

--- parser.c
+++ parser.c
@@ -1,5 +1,92 @@
 #include "parser.h"
 
+int size_table = 102;
+hashTable Table; 
+
+int hash(char *v, int M){ 
+	int h = 0, a = 257;
+    for (; *v != 0; v++)
+        h = (a*h + *v) % M;
+    return h;
+}
+
+void pupulateHashTable(){
+
+	Table = (hashTable)malloc(size_table*sizeof(hashNode*));
+	for(int i = 0;i < size_table;i++)
+	{
+		Table[i] = (hashNode*)malloc(sizeof(hashNode));
+		Table[i]->next == NULL;
+	}
+
+
+	int i = 0;
+
+	for(;i < num_of_nt;i++)
+	{
+		int hval = hash(ntArray[i],size_table);
+		if(Table[hval]->next == NULL)
+		{
+			Table[hval]->next = (hashNode*)malloc(sizeof(hashNode));
+			Table[hval]->next->index = i;
+			strcpy(Table[hval]->next->s, ntArray[i]);
+			Table[hval]->next->next = NULL;
+		}
+
+		else
+		{
+			hashNode* ptr = Table[hval]->next;
+			while(ptr != NULL && ptr->next != NULL)
+				ptr = ptr->next;
+
+			ptr->next = (hashNode*)malloc(sizeof(hashNode));
+			ptr->next->index = i;
+			strcpy(ptr->next->s, ntArray[i]);
+			ptr->next->next = NULL;
+		}
+	}
+
+	for(;i<size_table;i++)
+	{
+		int hval = hash(tokenArray[i-num_of_nt],size_table);
+		if(Table[hval]->next == NULL)
+		{
+			Table[hval]->next = (hashNode*)malloc(sizeof(hashNode));
+			Table[hval]->next->index = i;
+			strcpy(Table[hval]->next->s, tokenArray[i-num_of_nt]);
+			Table[hval]->next->next = NULL;
+		}
+
+		else
+		{
+			hashNode* ptr = Table[hval]->next;
+			while(ptr != NULL && ptr->next != NULL)
+				ptr = ptr->next;
+
+			ptr->next = (hashNode*)malloc(sizeof(hashNode));
+			ptr->next->index = i;
+			strcpy(ptr->next->s, tokenArray[i-num_of_nt]);
+			ptr->next->next = NULL;
+		}
+	}
+}
+
+int findIndex(char* s)
+{
+	int in = hash(s,size_table);
+
+	hashNode* ptr = Table[in];
+
+	while(ptr != NULL)
+	{
+		if(strcmp(ptr->s,s) == 0)
+			return ptr->index; 
+		ptr = ptr->next;
+	}
+
+	return -1;
+}
+
 void createParseTable(char* F, table T){
 	T = (table)malloc(num_of_nt*sizeof(int));
 
--- parser.h
+++ parser.h
@@ -57,22 +57,28 @@ typedef enum{
 	logicalOp,
 } nonTerminals;
 
-int no_of_t = 55, no_of_nt=47;
+int num_of_t = 55, num_of_nt=47;
 
 char* ntArray[] = {"factor","output_par","relationalOp","fieldDefinitions","arithmeticExpression","remaining_list","otherFunctions","typeDefinition","moreFields","declaration","inputParameters","dataType","funCallStmt","singleOrRecId","parameter_list","conditionalStmt","optionalReturn","global_or_not","outputParameters","op1","ioStmt","stmt","primitiveDatatype","function","otherStmts","ex2","declarations","iterativeStmt","constructedDatatype","assignmentStmt","returnStmt","B1","term","more_ids","typeDefinitions","allVar","C1","term2","program","mainFunction","op2","idList","stmts","fieldDefinition","input_par","booleanExpression","logicalOp"};
 
+char* symbolArray[] = {"factor","output_par","relationalOp","fieldDefinitions","arithmeticExpression","remaining_list","otherFunctions","typeDefinition","moreFields","declaration","inputParameters","dataType","funCallStmt","singleOrRecId","parameter_list","conditionalStmt","optionalReturn","global_or_not","outputParameters","op1","ioStmt","stmt","primitiveDatatype","function","otherStmts","ex2","declarations","iterativeStmt","constructedDatatype","assignmentStmt","returnStmt","B1","term","more_ids","typeDefinitions","allVar","C1","term2","program","mainFunction","op2","idList","stmts","fieldDefinition","input_par","booleanExpression","logicalOp","TK_ASSIGNOP",    "TK_COMMENT",    "TK_FIELDID",    "TK_ID",    "TK_NUM",    "TK_RNUM",    "TK_FUNID",    "TK_RECORDID",    "TK_WITH",    "TK_PARAMETERS",    "TK_END",    "TK_WHILE",    "TK_INT",    "TK_REAL",    "TK_TYPE",    "TK_MAIN",    "TK_GLOBAL",    "TK_PARAMETER",    "TK_LIST",    "TK_SQL",    "TK_SQR",    "TK_INPUT",    "TK_OUTPUT",    "TK_SEM",    "TK_COLON",    "TK_DOT",    "TK_COMMA",    "TK_ENDWHILE",    "TK_OP",    "TK_CL",    "TK_IF",    "TK_THEN",    "TK_ENDIF",    "TK_READ",    "TK_WRITE",    "TK_RETURN",    "TK_PLUS",    "TK_MINUS",    "TK_MUL",    "TK_DIV",    "TK_CALL",    "TK_RECORD",    "TK_ENDRECORD",    "TK_ELSE",    "TK_AND",    "TK_OR",    "TK_NOT",    "TK_LT",    "TK_LE",    "TK_EQ",    "TK_GT",    "TK_GE",    "TK_NE",    "TK_DOLLAR",    "eps"};
+
+
 typedef int** table;
-
-// typedef struct{
-// 	int arr[no_of_nt][no_of_t];
-
-// } Table;
 
 struct node{
 	int id;
 	struct node * next;
 };
 typedef struct node Node;
+
+typedef struct {
+	int index;
+	char s[MAX_LENGTH];
+	struct hashNode* next;
+}hashNode;
+
+typedef hashNode** hashTable;
 
 struct ruleRHS{
 	Node* head;
@@ -82,11 +88,13 @@ struct ruleRHS{
 typedef struct ruleRHS RuleRHS;
 
 struct grammar{
-	RuleRHS rules[no_of_nt];
+	RuleRHS rules[num_of_nt];
 	// int size;
 };
 
 typedef struct grammar Grammar;
 
 void createParseTable(char* F, table T);
+int hash(char *v, int M);
+
 #endif
