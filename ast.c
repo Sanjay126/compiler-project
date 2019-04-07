8843a57d88b680521f3d83b3b5ada0c8ac5d6ef6
Nilay Arora <nilayarora1298@gmail.com>
2019-04-07 12:46:59 +0530

    testing delete function

--- ast.c
+++ ast.c
@@ -70,3 +70,30 @@ ParseTree del_child(ParseTree node,int i)
 
 	return node;
 }
+
+int main()
+{
+	ParseTree nodes[7];
+	for(int i = 0;i < 7;i++)
+	{
+		ParseTree nodes[i] = (ParseTree)malloc(sizeof(parseTree));
+		nodes[i]->next = NULL;
+		nodes[i]->children = NULL;
+		nodes[i]->non_term_id = i;
+	}
+
+	node[1]->next = node[2];
+	node[2]->next = node[3];
+	node[4]->next = node[5];
+	node[5]->next = node[6];
+	node[2]->children = node[4];
+	node[0]->children = node[1];
+
+	printParseTree(node[0],"parseTree.txt");
+	del_child(node[0],2);
+	printParseTree(node[0],"ParseTree2.txt");
+	del_child(node[0],1);
+	printParseTree(node[0],"ParseTree3.txt");
+
+	return 0;
+}
